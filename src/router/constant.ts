e694057da757cc917cbf1c039cb021bfd4a28d6e
linpx <px.lin@qq.com>
2021-06-03 11:08:28 +0800

    u

--- src/router/constant.ts
+++ src/router/constant.ts
@@ -3,15 +3,6 @@ import { RouterItem } from '@/utils/interfaces';
 export const staticRoutes: RouterItem[] = [
   {
     path: '/',
-    redirect: '/portal',
-    component: () => import('@/views/portal/index.vue'),
-  },
-  {
-    path: '/portal',
-    component: () => import('@/views/portal/index.vue'),
-  },
-  {
-    path: '/portal/:username',
     component: () => import('@/views/portal/index.vue'),
   },
   {
